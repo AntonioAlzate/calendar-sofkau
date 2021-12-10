a9d0aee61c948880c68e07e3ac7d7dc675662002
Antonio Alzate <antonioalzate7@gmail.com>
2021-12-10 17:35:09 -0500

    Se llevaron a cabo los TODO del service método generateCalendar

--- src/main/java/co/com/sofka/calendar/services/SchedulerService.java
+++ src/main/java/co/com/sofka/calendar/services/SchedulerService.java
@@ -5,14 +5,13 @@ import co.com.sofka.calendar.model.ProgramDate;
 import co.com.sofka.calendar.repositories.ProgramRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -20,23 +19,25 @@ import java.util.stream.Stream;
 @Service
 public class SchedulerService {
 
+    public static final String PROGRAMA_NO_EXISTE = "El programa academnico no existe";
+
     @Autowired
     private ProgramRepository programRepository;
 
-    //TODO: deben retornar un flux de programDate Flux<ProgramDate>
-    public List<ProgramDate> generateCalendar(String programId, LocalDate startDate) {
+    // Completado --> TODO: deben retornar un flux de programDate Flux<ProgramDate>
+    public Flux<ProgramDate> generateCalendar(String programId, LocalDate startDate) {
         var endDate = new AtomicReference<>(LocalDate.from(startDate));
         final AtomicInteger[] pivot = {new AtomicInteger()};
         final int[] index = {0};
 
-        //TODO: debe pasarlo a reactivo, no puede trabaja elementos bloqueantes
-        //TODO: trabajar el map reactivo y no deben colectar
-        var program = programRepository.findById(programId).block();
-        return Optional.ofNullable(program)
-                .map(this::getDurationOf)
-                .orElseThrow(() -> new RuntimeException("El programa academnico no existe"))
+        // Completado -->TODO: debe pasarlo a reactivo, no puede trabaja elementos bloqueantes
+        // Completado -->TODO: trabajar el map reactivo y no deben colectar
+        var program = programRepository.findById(programId);
+
+        return program
+                .flatMapMany(p -> Flux.fromStream(getDurationOf(p)))
                 .map(toProgramDate(startDate, endDate, pivot[0], index))
-                .collect(Collectors.toList());
+                .switchIfEmpty(Mono.error(new RuntimeException(PROGRAMA_NO_EXISTE)));
     }
 
     //No tocar
